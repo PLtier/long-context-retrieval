a15c11b5a6f4fecadf15ba265b2cceba4619097e
Maciej Pawel Jalocha <maciej.jalocha.pl@gmail.com>
2026-04-16 18:47:30 +0200

    feat: new prompt idea

--- scripts/assure_queries.sh
+++ scripts/assure_queries.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 python lcr/generate_queries.py assure-queries \
-    --queries-base-dir ./data/processed/danish_dataset_short_poc/ \
+    --queries-base-dir ./data/processed/polish_poc_more \
     --datasets queries \
-    --save-path ./data/processed/danish_dataset_short_poc/checked_queries \
+    --save-path ./data/processed/polish_poc_more/checked_queries \
     --no-start-from-checkpoint \
-    --llm "OpenAI/gpt-oss-20B" \
-    --provider "together" \
+    --llm "OpenAI/gpt-oss-120B" \
+    --provider "together"
+
+python lcr/visualisation/visualise_jsonl.py data/processed/polish_poc_more/assurance_results.jsonl --preset assurance_results \
--- scripts/generate_queries.sh
+++ scripts/generate_queries.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 python lcr/generate_queries.py generate-queries \
-    --documents-base-dir ./data/interim/danish_dataset_short_poc \
+    --documents-base-dir ./data/interim/polish_poc_more \
     --datasets chunks \
-    --save-path ./data/processed/danish_dataset_short_poc/generated_queries \
+    --save-path ./data/processed/polish_poc_more/generated_queries \
     --no-start-from-checkpoint \
     --llm "OpenAI/gpt-oss-120B" \
     --provider "together" \
     --impl-context-col "implicit_context_chunks_ids" \
     --context-col "explicit_context_chunks_ids" \
+    --chain-context
+
+python lcr/visualisation/visualise_jsonl.py data/processed/polish_poc_more/queries.jsonl --preset queries
